c70feac4dde805b7e44478cd336c0682c37f93c1
leon <leonylcatwork@gmail.com>
2018-07-06 21:30:35 +0100

    Initial Commit

---
+++ MMCache/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MMCache
+//
+//  Created by leon on 06/07/2018.
+//  Copyright © 2018 leon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
